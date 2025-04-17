195c88bc7bd7e62273996366f9bb20afa1345c77
miaodhiambo <miaodhiambo@usiu.ac.ke>
2025-04-17 15:37:43 +0300

    Q1

---
+++ Q1.py
@@ -0,0 +1,16 @@
+#calculating salary
+salary(hourly_rate, hours_worked, tax_rate=0.15)
+
+gross_salary = hour_rate * hours_worked
+ 
+net_salary = gross_salary - tax_rate(15%)
+ return f"net salary
+
+
+calculate_salary = salary(soo, 40)
+print(salary)
+
+
+
+ 
+ 
